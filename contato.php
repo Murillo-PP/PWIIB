544f1b17539f8c6d6a3f77cfe0eb7c2a602747d9
Lab05_etec <Lab05_etec@etec>
2025-02-11 20:10:23 -0300

    Criação do arquivo Contato.php

--- contato.php
+++ contato.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto PWII B</title>
+    <title>Entre em contato</title>
 </head>
 <body>
     <?php
-        echo "<h1>Titulo do projeto PWII B</h1>";
+        echo "<h1>Página de contato do site</h1>";
     ?>
 </body>
 </html>
