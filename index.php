36c8c4b5db317f978f8830b3cfdd6751e681055f
Lab05_etec <Lab05_etec@etec>
2025-02-21 22:21:42 -0300

    https://github.com/Murillo-PP/PWIIB

--- index.php
+++ index.php
@@ -6,31 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Olá Mundo</title>
     <link href="bootstrap.min.css" rel="stylesheet">
-    <style>
-      .row{
-        border: 2px solid red;
-        padding: 5px;
-      }
-      .row > div{
-        border: 2px solid blue;
-        padding: 50px 0;
-        text-align: center;
-      }
-    </style>
 </head>
-
-<div class="row">
-    <div class="col-2">
-      <input class="btn btn-primary" type="button" value="Cadastrar">
-    </div>
-    <div class="col-8 offset-2">
-      <button class="btn btn-outline-success" type="submit">Pesquisa</button>
-    </div>
-</div>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+      <img src="img/REd.png" alt="Green Lantern" width="30" height="30">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -44,13 +26,15 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            PHP
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Variável</a></li>
+            <li><a class="dropdown-item" href="#">If</a></li>
+            <li><a class="dropdown-item" href="#">While</a></li>
+            <li><a class="dropdown-item" href="#">For</a></li>
+            <li><a class="dropdown-item" href="#">Swich</a></li>
+            <li><a class="dropdown-item" href="#">Array/Vetor</a></li>
           </ul>
         </li>
         <li class="nav-item">
@@ -64,82 +48,6 @@
     </div>
   </div>
 </nav>
-<div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-  <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </div>
-</div>
-    <!-- Example single danger button -->
-<div class="btn-group">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Menu
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-    <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#">Separated link</a></li>
-  </ul>
-</div>
-    <div class="alert alert-success" role="alert">
-        Alerta boostrap
-    </div>
-    <a href="index.php">Home</a>
-    <a href="contato.php">Página de contato</a>
-    <a href="produtos.php">Lista de produtos</a>
-    <?php
-        echo "<h1>Título do projeto PWII ETEC</h1>";
-    ?>
-    <button type="button" class="btn btn-primary">Botão bootstrap</button>
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Abrir modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Tem certeza que quer sair?</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Tem certeza que quer sair do sistema?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Não sair</button>
-        <button type="button" class="btn btn-success">Sair</button>
-      </div>
-    </div>
-  </div>
-</div>
-<ul class="list-group list-group-flush">
-  <li class="list-group-item">Primeiro item</li>
-  <li class="list-group-item">Segundo item</li>
-  <li class="list-group-item">Terceiro item da lista</li>
-  <li class="list-group-item">Quarto item da lista</li>
-  <li class="list-group-item">Quinto item da lista</li>
-</ul>
-    <script src="bootstrap.bundle.min.js" ></script>
+  <script src="bootstrap.bundle.min.js" ></script>
 </body>
 </html>
